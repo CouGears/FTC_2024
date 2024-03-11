f217ca9d63f179cdfb9333ed99af12e0e0e25d7d
Jed <joshuaelijah.davis@gmail.com>
2024-03-11 11:48:59 -0400

    disabled liftLimitTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/liftLimitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/liftLimitTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Components;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,6 +10,7 @@ import org.firstinspires.ftc.teamcode.AutonMethods;
 
 //TODO: Uncomment the following line to use
 @TeleOp
+@Disabled
 public class liftLimitTest extends LinearOpMode {
 
     public static DcMotor Lift;
